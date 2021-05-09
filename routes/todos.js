52ebf12a8d556b307acea8185edaf755b6de3b03
gulyaev <gulyaev.gvs@gmail.com>
2021-05-09 23:08:11 +0200

    Todo list

---
+++ routes/todos.js
@@ -0,0 +1,43 @@
+const { Router } = require('express');
+const Todo = require('../models/Todo');
+
+const router = Router();
+
+//const bodyParser = require('body-parser');
+//const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+router.get('/', async (req, res) => {
+    const todos = await Todo.find({}).lean();
+    res.render('index', {
+        title: 'todos list',
+        isIndex: true,
+        todos
+    });
+});
+
+router.get('/create', (req, res) => {
+    res.render('create', {
+        title: 'create todo',
+        isCreate: true
+    });
+});
+
+router.post('/create', async (req, res) => {
+    const todo = new Todo({
+        title: req.body.title
+    });
+
+    await todo.save()
+    res.redirect('/')
+})
+
+router.post('/complete', async (req, res) => {
+    const todo = await Todo.findById(req.body.id);
+
+    todo.completed = !!req.body.completed;
+
+    await todo.save();
+    res.redirect('/')
+})
+
+module.exports = router;
